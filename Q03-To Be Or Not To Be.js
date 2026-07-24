603be37511df737957735ad0a6944945c81a403c
Lalbabu-Coder <rishisingh765453@gmail.com>
2026-07-24 14:45:23 +0530

    Leet code question 3

---
+++ Q03-To Be Or Not To Be.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: (val2) => {
+            if (val === val2) return true;
+            throw new Error ("Not Equal");
+
+        },
+        notToBe: (val2) =>{
+            if (val !== val2) return true;
+            throw new Error("Equal");
+        
+        }
+    };
+    
+};
